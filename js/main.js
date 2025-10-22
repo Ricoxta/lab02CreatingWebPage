9988aa3677bbb096685e34161565a98822fe8719
ricoxta <ricota.uw.edu>
2025-10-22 02:39:10 -0700

    changed the navbar color and changed the caption position to be bot center of images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 // Get the element with the class "icon"
 let icon = document.getElementsByClassName("icon")[0];
+let slideIndex = 1;
+showSlides(slideIndex)
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideshow");
+  if(n > slides.length) {
+    slideIndex = 1;
+  }
+  if(n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
 // Add an event listener for the 'click' event on the icon element
 icon.addEventListener('click', responsive_control);
 
@@ -18,3 +38,5 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+
+
