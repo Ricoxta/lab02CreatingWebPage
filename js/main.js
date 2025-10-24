fd5e3020492bc2c58f9bd25778c399c0a3c3048c
ricoxta <ricota.uw.edu>
2025-10-23 17:46:39 -0700

    created cuisine cards for cooking section + done w/cooking section aka section 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 // Get the element with the class "icon"
 let icon = document.getElementsByClassName("icon")[0];
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+    console.log("added responsive");
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+    console.log("turned into topnav")
+  }
+}
+
 let slideIndex = 1;
 showSlides(slideIndex)
 
@@ -21,22 +42,3 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
-// Add an event listener for the 'click' event on the icon element
-icon.addEventListener('click', responsive_control);
-
-// Function to control the responsiveness of the navigation bar
-function responsive_control() {
-  // Get the element with the id "myTopnav"
-  let x = document.getElementById("myTopnav");
-
-  // Check if the class name of the element is "topnav"
-  if (x.className === "topnav") {
-    // If it is, add the "responsive" class to the element
-    x.className += " responsive";
-  } else {
-    // If it's not, remove the "responsive" class from the element
-    x.className = "topnav";
-  }
-}
-
-
